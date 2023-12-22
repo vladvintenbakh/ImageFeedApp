ae1d3c258d6b75f5d65e45da225b4dcd0f26fefb
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2023-12-22 19:44:03 +0600

    Set up the single image view screen and segue

---
+++ ImageFeedApp/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeedApp
+//
+//  Created by Vladislav Vintenbakh on 22/12/23.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    override func viewDidLoad() {
+        imageView.image = image
+    }
+}
