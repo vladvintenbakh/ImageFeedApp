b6f2d58898a0f8052341baa0874a15bdfc5db76b
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2023-12-27 09:43:03 +0600

    Changed the date formatter as per the Figma layout

--- ImageFeedApp/ImageList/ImageListViewController.swift
+++ ImageFeedApp/ImageList/ImageListViewController.swift
@@ -15,8 +15,8 @@ final class ImageListViewController: UIViewController {
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
+        formatter.dateFormat = "dd MMMM yyyy"
+        formatter.locale = Locale(identifier: "ru_RU")
         return formatter
     }()
     
