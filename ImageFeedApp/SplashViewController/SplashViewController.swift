c328575b1fcab68699271530b11aa4279c00207b
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-01-17 18:32:43 +0600

    Added UIBlockingProgressHUD

--- ImageFeedApp/SplashViewController/SplashViewController.swift
+++ ImageFeedApp/SplashViewController/SplashViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import ProgressHUD
 
 final class SplashViewController: UIViewController {
     
@@ -49,7 +48,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.show()
+        UIBlockingProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -62,9 +61,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
-                ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
             case .failure:
-                ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
                 // TODO: Process the error
             }
         }
