8f973bf4632c0d23d41264f1b88c97aebb5339cf
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-01-17 18:44:46 +0600

    Added back the UIBlockingProgressHUD class

--- ImageFeedApp/UIBlockingProgressHUD.swift
+++ ImageFeedApp/UIBlockingProgressHUD.swift
@@ -5,4 +5,21 @@
 //  Created by Vladislav Vintenbakh on 17/1/24.
 //
 
-import Foundation
+import UIKit
+import ProgressHUD
+
+final class UIBlockingProgressHUD {
+    private static var window: UIWindow? {
+        return UIApplication.shared.windows.first
+    }
+    
+    static func show() {
+        window?.isUserInteractionEnabled = false
+        ProgressHUD.show()
+    }
+    
+    static func dismiss() {
+        window?.isUserInteractionEnabled = true
+        ProgressHUD.dismiss()
+    }
+}
