e959adb242c3d0948fc476045575132e82b7d5c9
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-01-17 18:14:35 +0600

    Made changes to fetchAuthToken to avoid race condition

--- ImageFeedApp/ServiceAccessLayer/OAuth2/OAuth2Service.swift
+++ ImageFeedApp/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -13,6 +13,9 @@ final class OAuth2Service {
     
     private let urlSession = URLSession.shared
     
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
     private (set) var authToken: String? {
         get {
             return OAuth2TokenStorage().token
@@ -24,6 +27,10 @@ final class OAuth2Service {
     
     func fetchAuthToken(_ code: String,
                         completion: @escaping (Result<String, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        if lastCode == code { return }
+        task?.cancel()
+        lastCode = code
         let request = authTokenRequest(code: code)
         let task = object(for: request) { [weak self] result in
             guard let self = self else { return }
@@ -32,10 +39,14 @@ final class OAuth2Service {
                 let authToken = body.accessToken
                 self.authToken = authToken
                 completion(.success(authToken))
+                self.task = nil
             case .failure(let error):
                 completion(.failure(error))
+                self.task = nil
+                self.lastCode = nil
             }
         }
+        self.task = task
         task.resume()
     }
 }
