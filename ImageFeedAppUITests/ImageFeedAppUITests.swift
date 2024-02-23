2ec2f57597d41b5dbd09fb339c1446ecb048c3e9
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-02-23 11:10:37 +0800

    Added a target for UI tests

---
+++ ImageFeedAppUITests/ImageFeedAppUITests.swift
@@ -0,0 +1,30 @@
+//
+//  ImageFeedAppUITests.swift
+//  ImageFeedAppUITests
+//
+//  Created by Vlad Vintenbakh on 23/2/24.
+//
+
+import XCTest
+
+final class ImageFeedAppUITests: XCTestCase {
+    private let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        
+        app.launch()
+    }
+    
+    func testAuth() throws {
+        
+    }
+    
+    func testFeed() throws {
+        
+    }
+    
+    func testProfile() throws {
+        
+    }
+}
